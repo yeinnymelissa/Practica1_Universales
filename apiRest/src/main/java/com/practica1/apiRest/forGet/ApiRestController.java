7c7667550bf0100cfe4fd44ad5d27f374eaaa731
yeinnymelissa <3017261750101@ingenieria.usac.edu.gt>
2023-01-14 01:21:41 -0600

    Las dos apis

--- apiRest/src/main/java/com/practica1/apiRest/forGet/ApiRestController.java
+++ apiRest/src/main/java/com/practica1/apiRest/forGet/ApiRestController.java
@@ -7,6 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo.None;
+import com.fasterxml.jackson.annotation.Nulls;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.nio.channels.NonReadableChannelException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.lang.model.type.NullType;
+
+import org.json.JSONObject;
+import org.hamcrest.core.IsNull;
+import org.json.JSONArray;
+import org.json.simple.parser.JSONParser;
+
 @RestController
 @RequestMapping("/rest")
 @CrossOrigin
@@ -28,7 +44,9 @@ public class ApiRestController {
 	//https://itunes.apple.com/search?term=katy+perry&attribute=allArtistTerm
 	
 	@GetMapping(value = "/find/{nameC}")
-	private String findTodo(@PathVariable("nameC") String nameC) {
+	private List findTodo(@PathVariable("nameC") String nameC) {
+		JSONParser parser = new JSONParser();
+		
 		String[] splitName = nameC.split("%20");
 		
 		String nameSinger = "";
@@ -40,9 +58,65 @@ public class ApiRestController {
 		    	  nameSinger += "+" + splitName[i];
 		      }
 		}
-		String linkITunes = "https://itunes.apple.com/search?term="+nameSinger+"&attribute=allArtistTerm";
 		RestTemplate restTemplate = new RestTemplate();
+		String linkITunes = "https://itunes.apple.com/search?term="+nameSinger+"&attribute=allArtistTerm";
 		String result = restTemplate.getForObject(linkITunes, String.class);
-		return result;
+		
+		
+		String linkTVMaze = "https://api.tvmaze.com/search/people?q="+nameC;
+		String resultTV = restTemplate.getForObject(linkTVMaze, String.class);
+
+		
+		JSONObject jsonITunes = new JSONObject(result); 
+		ArrayList<InfoPeople> listPeople = new ArrayList<InfoPeople>();
+		
+		JSONArray objectInfo = (JSONArray) jsonITunes.get("results");
+		
+		int cont = 0;
+        while(cont < objectInfo.length()){
+            JSONObject person = (JSONObject) objectInfo.get(cont);
+            String name = person.get("artistName").toString();
+            String track = person.get("trackName").toString();
+            String type = person.get("kind").toString();
+            String service = "API ITunes";
+            InfoPeople one = new InfoPeople(name, track, type, service, linkITunes);
+            listPeople.add(one);
+            cont++;
+        }
+        
+        JSONArray jsonTVMaze = new JSONArray(resultTV);
+        
+        int contTV = 0;
+        while(contTV < jsonTVMaze.length()){
+            JSONObject score = (JSONObject) jsonTVMaze.get(contTV);
+            JSONObject person = (JSONObject) score.get("person");
+            String name = person.get("name").toString();
+            String type = "People";
+            String service = "API TVMaze";
+            InfoPeople one = new InfoPeople(name, null, type, service, linkTVMaze);
+            listPeople.add(one);
+            contTV++;
+        }
+        
+        JSONArray list = new JSONArray();
+        
+		for(InfoPeople people : listPeople) {
+			JSONObject person = new JSONObject();
+			
+			person.put("type", people.type);
+			person.put("service", people.service);
+			person.put("name", people.name);
+			person.put("serviceUrl", people.serviceURL);
+			if(people.trackname == null) {
+				person.put("trackName", JSONObject.NULL);
+			}else {
+				person.put("trackName", people.trackname);
+			}
+			
+
+			list.put(person);
+			
+		}
+		return list.toList();
 	}
 }
---
+++ apiRest/src/main/java/com/practica1/apiRest/forGet/InfoPeople.java
@@ -0,0 +1,20 @@
+package com.practica1.apiRest.forGet;
+
+public class InfoPeople {
+	
+	public String name;
+	public String trackname;
+	public String type;
+	public String service;
+	public String serviceURL;
+	
+	public InfoPeople(String name, String trackname, String type, String service, String serviceURL) {
+		super();
+		this.name = name;
+		this.trackname = trackname;
+		this.type = type;
+		this.service = service;
+		this.serviceURL = serviceURL;
+	}
+	
+}
